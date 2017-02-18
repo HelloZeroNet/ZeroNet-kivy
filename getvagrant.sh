ff216bc49122dfa083c06493e99a92e046de6b0a
mkg20001 <mkg20001@gmail.com>
2017-02-18 13:34:43 +0100

    Update to latest vagrant

---
+++ getvagrant.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mir="http://archive.ubuntu.com"
+url="$mir/ubuntu/pool/universe/v/vagrant/"
+
+file_=$(curl $url --silent | grep "vagrant_[0-9.+a-z_-]*" -o | awk '!x[$0]++' | grep "deb$" | sort -r | head -n 1)
+file="$url$file_"
+
+o="vagrant.deb"
+
+set -x
+
+wget $file -O $o
+dpkg -i $o
+apt-get install -f
