9c860f342ce0ccdcc8c30a7d376864d216fadcf5
mkg20001 <mkg20001@gmail.com>
2017-02-18 13:54:04 +0100

    Use deb from getvagrant.com

--- getvagrant.sh
+++ getvagrant.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-url="https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.deb"
+file="https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.deb"
 
 o="vagrant.deb"
 
