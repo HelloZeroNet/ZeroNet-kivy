7aa8a29d95528964e1c03603e603205bdb47d4ea
Maciej Krüger <mkg20001@gmail.com>
2019-02-27 13:57:54 +0100

    fix: metadata generation

--- .ci/release.sh
+++ .ci/release.sh
@@ -13,9 +13,13 @@ for arch_path in ci-out/*; do
   debug=$(echo "$arch_path/"*debug*)
 
   for f in "$release_unaligned" "$debug"; do
-    fout=${f/".apk"/"$arch.apk"}
+    fout=${f/".apk"/"-$arch.apk"}
     mv -v "$f" "${fout}"
   done
 done
 
-bash ./tool.sh metadata
+getmeta() {
+  cat src/zero/src/Config.py | grep "$1 =" | sed -r "s|(.*) = ||g"
+}
+
+echo "{\"rev\":$(getmeta self.rev),\"ver\":$(getmeta self.version),\"date\":$(expr $(date +%s) \* 1000)}" > release/metadata.json
