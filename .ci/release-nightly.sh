1afd71a481990bd0a9ded4fdc7f3b57be9c5705a
Maciej Krüger <mkg20001@gmail.com>
2019-03-03 23:46:49 +0100

    fix(ci): ..again

--- .ci/release-nightly.sh
+++ .ci/release-nightly.sh
@@ -2,7 +2,7 @@
 
 for f in package/*release*.apk; do
   OUT=${f/"package/"/"release/"}
-  curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@$f" -O "$OUT"
+  curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@$f" -o "$OUT"
 done
 
 # TODO: push to repo
