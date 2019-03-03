73d6eb106fe9a9e73412087b438b185afa398a19
Maciej Krüger <mkg20001@gmail.com>
2019-03-04 00:48:14 +0100

    misc: ...see previous

--- .ci/release-nightly.sh
+++ .ci/release-nightly.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for f in package/*release*.apk; do
+for f in package/unsigned/*.apk; do
   OUT=${f/"package/"/"release/"}
+  OUT=${OUT/"-unsigned"/"-nightly"}
   curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@$f" -o "$OUT"
 done
 
