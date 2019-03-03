7dea4fdcd7b9ed37f1f84a4a8a2c4ae89c328384
Maciej Krüger <mkg20001@gmail.com>
2019-03-03 13:02:39 +0100

    feat: better release process

---
+++ .ci/build.sh
@@ -0,0 +1,42 @@
+'use strict'
+
+set -ev
+
+# prepare
+bash .ci/bind-cache.sh
+export TERM=xterm-256color
+export DISABLE_PROGRESS=1
+# source .version.sh
+
+replace_var() {
+  sed -r "s|#* *$1 = .*|$1 = $2|g" -i buildozer.spec
+}
+
+disable_var() {
+  sed -r "s|#* *$1 = .*|# $1 (disabled) = 0|g" -i buildozer.spec
+}
+
+if [ -z "$CI_COMMIT_TAG" ]; then # build a nightly
+  replace_var package.domain luna.mkg20001
+  replace_var title ZeroNetN
+  # disable_var version.regex
+  # disable_var version.filename
+  # replace_var version "$CUR_VERSION.$(date +%s)"
+  # SUFFIX=$(git log --oneline | wc -l | sed -r "s|(.+)([0-9]{2})|\1.\2|g") # turns commit count into version, e.g. 111 -> 1.11
+  SUFFIX=$(git log --oneline | wc -l) # counts commits
+  sed -r "s|VER_SUFFIX.+|VER_SUFFIX=$SUFFIX|g" -i .version.sh
+fi
+
+# setup
+replace_var android.arch "$TARGET_ARCH"
+echo -e "EXEC=docker\nDOCKER_IMAGE=$IMAGE_TAG\nDISABLE_PROGRESS=1" > .env
+docker pull $IMAGE_TAG
+bash ./tool.sh prebuild
+
+# execute
+script -q -e -c "make _ci"
+
+# post-check
+ls bin | grep debug | grep .apk # ensure job generated debug apk
+ls bin | grep release | grep .apk # ensure job generated release apk
+mkdir ci-out && mv bin ci-out/$CI_JOB_NAME && mv buildozer.spec ci-out/$CI_JOB_NAME/
--- .ci/package.sh
+++ .ci/package.sh
@@ -2,20 +2,24 @@
 
 set -e
 
+mkdir -p release/{release,debug,spec}
+
 for arch_path in ci-out/*; do
   arch=$(basename "$arch_path")
-  mkdir -p "release/$arch"
 
   # generate stripped spec file
-  cat "$arch_path/buildozer.spec" | sed "s|^#.+||g" | grep -v "^\$" > "release/$arch/buildozer.spec"
+  cat "$arch_path/buildozer.spec" | sed "s|^#.+||g" | grep -v "^\$" > "package/spec/buildozer.$arch.spec"
 
+  # get apks
   release_unaligned=$(echo "$arch_path/"*release-unsigned*)
   debug=$(echo "$arch_path/"*debug*)
 
+  # move apks
   for f in "$release_unaligned" "$debug"; do
+    outfolder=$(echo "$f" | sed "s|-unaligned||g" | sed -r "s|.+-([a-z-]+).apk|\1|g")
     fout=${f/".apk"/"-$arch.apk"}
     fout=$(basename "$fout")
-    fout="release/$fout"
+    fout="package/$outfolder/$fout"
     mv -v "$f" "${fout}"
   done
 done
@@ -24,4 +28,4 @@ getmeta() {
   cat src/zero/src/Config.py | grep "$1 =" | sed -r "s|(.*) = ||g"
 }
 
-echo "{\"rev\":$(getmeta self.rev),\"ver\":$(getmeta self.version),\"date\":$(expr $(date +%s) \* 1000)}" > release/metadata.json
+echo "{\"rev\":$(getmeta self.rev),\"ver\":$(getmeta self.version),\"date\":$(expr $(date +%s) \* 1000)}" > package/metadata.json
