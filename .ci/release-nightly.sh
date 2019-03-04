15971c72bbba278ba758ddc07edd41780dc09806
Maciej Krüger <mkg20001@gmail.com>
2019-03-04 16:01:39 +0100

    feat: upload to f-droid repo

--- .ci/release-nightly.sh
+++ .ci/release-nightly.sh
@@ -2,10 +2,13 @@
 
 mkdir release
 
+UPCMD=$(echo "$NIGHTLY_UPCMD" | base64 -d)
+
 for f in package/unsigned/*.apk; do
   OUT=${f/"package/unsigned/"/"release/"}
   OUT=${OUT/"-unsigned"/"-nightly"}
   curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@$f" -o "$OUT"
+  $UPCMD -F "apks=@$OUT"
 done
 
 # TODO: push to repo
