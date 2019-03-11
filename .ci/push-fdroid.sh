df27c21d79f24f510cbfdcabda2ab2ec36318944
Maciej Krüger <mkg20001@gmail.com>
2019-03-11 23:16:40 +0100

    feat(push-to-fdroid): push all apks at once

--- .ci/push-fdroid.sh
+++ .ci/push-fdroid.sh
@@ -2,6 +2,10 @@
 
 cd release
 
+F=()
+
 for apk in *; do
-  curl 'https://f-droid.mkg20001.io/1/' --compressed -H "$FDROID_TOKEN_1" -H "$FDROID_TOKEN_2" -H 'RM-Background-Type: apks' -F "apks=@$apk"
+  F+=(-F "apks=@$apk")
 done
+
+curl 'https://f-droid.mkg20001.io/1/' --compressed -H "$FDROID_TOKEN_1" -H "$FDROID_TOKEN_2" -H 'RM-Background-Type: apks' "${F[@]}"
