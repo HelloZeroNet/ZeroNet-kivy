40850cc6bdc4eea77e18afb70efe7b2e107eccb7
Maciej Krüger <mkg20001@gmail.com>
2019-03-11 21:58:07 +0100

    fix: remove UPCMD rev

--- .ci/release-nightly.sh
+++ .ci/release-nightly.sh
@@ -8,7 +8,4 @@ for f in package/unsigned/*.apk; do
   OUT=${f/"package/unsigned/"/"release/"}
   OUT=${OUT/"-unsigned"/"-nightly"}
   curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@$f" -o "$OUT"
-  $UPCMD -F "apks=@$OUT"
 done
-
-# TODO: push to repo
