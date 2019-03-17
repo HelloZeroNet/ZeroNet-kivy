20ea8e9041824cb22c03227054499bcb1bd6f34e
Maciej Krüger <mkg20001@gmail.com>
2019-03-17 13:29:10 +0100

    fix: align apk before upload

--- .ci/release-nightly.sh
+++ .ci/release-nightly.sh
@@ -7,7 +7,12 @@ UPCMD=$(echo "$NIGHTLY_UPCMD" | base64 -d)
 for f in package/unsigned/*.apk; do
   OUT=${f/"package/unsigned/"/"release/"}
   OUT=${OUT/"-unsigned"/"-nightly"}
-  curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@$f" -o "$OUT"
+
+  rm -f /tmp/zn-release.apk
+  zipalign -v -p 4 "$f" /tmp/zn-release.apk
+
+  curl 172.17.0.1:6234 --header "Token: $NIGHTLY_SIGNING_TOKEN" -F "apk=@/tmp/zn-release.apk" -o "$OUT"
+
   if [ ! -s "$OUT" ]; then
     echo "ERR: Signing failed" >&2
     exit 2
