def33cc508bfd65981b5b05ad75258262748697c
Maciej Krüger <mkg20001@gmail.com>
2019-03-17 13:30:34 +0100

    fix: detect failed signing

--- .ci/push-fdroid.sh
+++ .ci/push-fdroid.sh
@@ -8,4 +8,10 @@ for apk in *; do
   F+=(-F "apks=@$apk")
 done
 
-curl 'https://f-droid.mkg20001.io/1/' --compressed -H "$FDROID_TOKEN_1" -H "$FDROID_TOKEN_2" -H 'RM-Background-Type: apks' "${F[@]}"
+OUT=$(curl 'https://f-droid.mkg20001.io/1/' --compressed -H "$FDROID_TOKEN_1" -H "$FDROID_TOKEN_2" -H 'RM-Background-Type: apks' "${F[@]}")
+
+if [ ! -z "$OUT" ]; then
+  echo "$OUT" >&2
+  echo "Signing failed!" >&2
+  exit 2
+fi
