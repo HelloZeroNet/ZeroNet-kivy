2dc2421a15e26b43a46eff89b85d97c59a9d4dd6
Maciej Krüger <mkg20001@gmail.com>
2019-03-17 15:31:08 +0100

    fix: notifis for real

--- src/service.py
+++ src/service.py
@@ -21,13 +21,15 @@ if env["platform"] == "android":
     notifi.setContentText("ZeroNet is running")
 
     if SDK_INT >= 26:
-        manager = autoclass('android.app.NotificationManager')
+        manager = autoclass('android.app.NotificationManager') # manager is NotificationManager
         channel = autoclass('android.app.NotificationChannel')
+        managerID = autoclass('android.content.Context').NOTIFICATION_SERVICE
 
-        app_channel = channel(
-          "service_zn", "ZeroNet Background Service", manager.IMPORTANCE_MIN
+        app_channel = channel( # val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+          "service_zn", "ZeroNet Background Service", manager.IMPORTANCE_MIN # val importance = NotificationManager.IMPORTANCE_DEFAULT
         )
-        Service.getSystemService(manager).createNotificationChannel(app_channel)
+        Service.getSystemService(managerID).createNotificationChannel(app_channel) # val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        # notificationManager.createNotificationChannel(mChannel)
         notifi.setChannel("service_zn")
 #    if SDK_INT >= 28:
 #        Service.startForeground(233,notifi)
