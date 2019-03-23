62cde0dcfd8780d25d47a966c1ece2f6650486e3
Maciej Krüger <mkg20001@gmail.com>
2019-03-23 11:28:07 +0100

    feat: update toolchains

---
+++ vendor/update-vendor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for dep in buildozer python-for-android; do
+	pushd $dep
+	if ! git remote -v | grep upstream >/dev/null; then
+		git remote set-url origin git@github.com:mkg20001/$dep
+		git remote add upstream git@github.com:kivy/$dep
+	fi
+	git fetch -p
+	git branch -d master
+	git reset --hard origin/master
+	git checkout -b master
+	git rebase upstream/master
+	bash
+	if [ -e .ok ]; then
+		git push origin HEAD:$(date +%s)
+		git push -uf origin master
+		rm .ok
+	fi
+	popd
+done
