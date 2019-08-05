76922041dcf9993aef04505ef09e3f6a4e6ed52e
Maciej Krüger <mkg20001@gmail.com>
2019-08-05 16:40:00 +0200

    fix: fdroid push

--- .ci/push-fdroid.sh
+++ .ci/push-fdroid.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
+set -e
+
 cd release
 
-F=()
+CMD=$(echo "$FDROID_UPLOAD" | base64 -d)
 
-for apk in *; do
-  F+=(-F "apks=@$apk")
-done
-
-OUT=$(curl 'https://f-droid.mkg20001.io/1/' --compressed -H "$FDROID_TOKEN_1" -H "$FDROID_TOKEN_2" -H 'RM-Background-Type: apks' "${F[@]}")
-
-if [ ! -z "$OUT" ]; then
-  echo "$OUT" >&2
-  echo "Signing failed!" >&2
-  exit 2
-fi
+eval $CMD
